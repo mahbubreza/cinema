79b1e2e6638bf4621054b031aa665ad6d850faf2
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-10-01 00:16:27 +0600

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Sun from "./assets/icons/sun.svg";
+import Logo from "./assets/logo.svg";
+import Ring from "./assets/ring.svg";
+import ShoppingCart from "./assets/shopping-cart.svg";
+import CardDetails from "./cine/CardDetails";
+
+export default function Header() {
+  const [showCart, setShowCart] = useState(false);
+
+  function handleCartShow() {
+    setShowCart(true);
+  }
+  return (
+    <header>
+      {showCart && <CardDetails />}
+      <nav className="container flex items-center justify-between space-x-10 py-6">
+        <a href="index.html">
+          <img src={Logo} width="139" height="26" alt="" />
+        </a>
+
+        <ul className="flex items-center space-x-5">
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Ring} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={Sun} width="24" height="24" alt="" />
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={handleCartShow}
+              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+              href="#"
+            >
+              <img src={ShoppingCart} width="24" height="24" alt="" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
